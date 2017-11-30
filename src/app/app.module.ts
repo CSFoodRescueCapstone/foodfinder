aa9d733ece34258d8110a13ef078dc9bd0fa5bac
Laura Himelman <laura.himelman@coloradocollege.edu>
2017-11-30 22:39:24 +0000

    fixed tabs in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { AddPostPage } from '../pages/addpost/addpost';
+import { ProfilePage } from '../pages/profile/profile';
+import { FeedPage } from '../pages/feed/feed';
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
@@ -19,9 +19,9 @@ import { AngularFireDatabaseModule } from 'angularfire2/database'; // LH added f
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    AddPostPage,
+    ProfilePage,
+    FeedPage,
     TabsPage
   ],
   imports: [
@@ -33,9 +33,9 @@ import { AngularFireDatabaseModule } from 'angularfire2/database'; // LH added f
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
+    AddPostPage,
+    ProfilePage,
+    FeedPage,
     TabsPage
   ],
   providers: [
