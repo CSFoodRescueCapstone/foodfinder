a8ccd5635bc2372482f01e27126dc336bba9c807
Laura Himelman <laura.himelman@coloradocollege.edu>
2017-11-30 22:15:16 +0000

    add angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { AngularFireModule } from 'angularfire2'; // LH added for add angularfire
+import { FIREBASE_CONFIG } from './firebase.credentials'; // LH added for add angularfire
+import { AngularFireDatabaseModule } from 'angularfire2/database'; // LH added for add angularfire
+
+
 @NgModule({
   declarations: [
     MyApp,
@@ -21,7 +26,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG), // LH added for add angularfire
+    AngularFireDatabaseModule // LH added for add angularfire
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCRPJax5158-1xvS-LZ26Bf9aCuAQmh_GY",
+    authDomain: "food-finder-279f6.firebaseapp.com",
+    databaseURL: "https://food-finder-279f6.firebaseio.com",
+    projectId: "food-finder-279f6",
+    storageBucket: "",
+    messagingSenderId: "733234564288"
+  };
