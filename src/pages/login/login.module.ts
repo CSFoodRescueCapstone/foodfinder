b3864e4b3b7bf83d811ce00c120b37fd781c260e
Laura Himelman <laura.himelman@coloradocollege.edu>
2017-12-01 00:40:27 +0000

    added entry point for register page

--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
-import { RegisterPage } from '../register/register';
 
 @NgModule({
   declarations: [
-    LoginPage,
-    RegisterPage
+    LoginPage
   ],
   imports: [
-    IonicPageModule.forChild(LoginPage),
-    IonicPageModule.forChild(RegisterPage)
+    IonicPageModule.forChild(LoginPage)
   ],
 })
 export class LoginPageModule {}
--- src/pages/register/register.module.ts
+++ src/pages/register/register.module.ts
@@ -9,5 +9,8 @@ import { RegisterPage } from './register';
   imports: [
     IonicPageModule.forChild(RegisterPage),
   ],
+  entryComponents: [
+    RegisterPage
+  ]
 })
 export class RegisterPageModule {}
