14ae4bc3db9ee38d8e4d9a91cbd30c46530c8690
Laura Himelman <laura.himelman@coloradocollege.edu>
2017-12-01 00:28:59 +0000

    added register page to login module

--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
+import { RegisterPage } from './register';
 
 @NgModule({
   declarations: [
     LoginPage,
+    RegisterPage
   ],
   imports: [
     IonicPageModule.forChild(LoginPage),
+    IonicPageModule.forChild(RegisterPage)
   ],
 })
 export class LoginPageModule {}
