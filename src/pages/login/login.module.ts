e6be98c04241a5927d7f9aabe877012b804d5f03
Laura Himelman <laura.himelman@coloradocollege.edu>
2017-12-01 00:31:37 +0000

    imported register page correctly

--- src/pages/login/login.module.ts
+++ src/pages/login/login.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { LoginPage } from './login';
-import { RegisterPage } from './register';
+import { RegisterPage } from '../register/register';
 
 @NgModule({
   declarations: [
