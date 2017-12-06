b50d0715017111e791ea2baddd6c7dc8e742b852
Laura Himelman <laura.himelman@coloradocollege.edu>
2017-12-06 22:31:20 +0000

    again adding button for add photo from library

--- src/pages/addpost/addpost.ts
+++ src/pages/addpost/addpost.ts
@@ -57,7 +57,23 @@ export class AddPostPage {
     
   }
   
-  addLibraryPhoto() {
+  addLibraryPhoto () {
+    
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: 0
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+    // imageData is either a base64 encoded string or a file URI
+    // If it's base64:
+    let base64Image = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+    // Handle error
+    });
     
   }
   
