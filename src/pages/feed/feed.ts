93f00550e8079ef93d047a618aa27f358bcd9c8b
Laura Himelman <laura.himelman@coloradocollege.edu>
2017-12-06 06:41:35 +0000

    posts show up in feed

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,20 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html'
 })
 export class FeedPage {
+  posts$: Observable<any[]>;
 
-  constructor(public navCtrl: NavController) {
-    this.loadFeed();
-  }
-  
-  loadFeed() {
-    // var posts = this.af.ref('posts/');
-    // posts.on('value', function(snapshot) {
-    //   console.log(snapshot.val());
-    // });
+  constructor(private af: AngularFireDatabase, public navCtrl: NavController) {
+    this.posts$ = this.af.list('/posts').valueChanges();
   }
 }
