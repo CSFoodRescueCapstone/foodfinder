0ccdc044d0f626c2e95b323a7942d0bca41c6c7a
Laura Himelman <laura.himelman@coloradocollege.edu>
2017-12-15 12:20:53 +0000

    conference data stoppe working, commented out maps stuff

--- src/providers/conference-data.ts
+++ src/providers/conference-data.ts
@@ -10,36 +10,36 @@ export class ConferenceData {
 
   constructor(public http: Http) { }
 
-  load(): any {
-    if (this.data) {
-      return Observable.of(this.data);
-    } else {
-      return this.http.get('assets/data/data.json')
-        .map(this.processData, this);
-    }
-  }
+//   load(): any {
+//     if (this.data) {
+//       return Observable.of(this.data);
+//     } else {
+//       return this.http.get('assets/data/data.json')
+//         .map(this.processData, this);
+//     }
+//   }
   
 
-  getSpeakers() {
-    return this.load().map((data: any) => {
-      return data.speakers.sort((a: any, b: any) => {
-        let aName = a.name.split(' ').pop();
-        let bName = b.name.split(' ').pop();
-        return aName.localeCompare(bName);
-      });
-    });
-  }
+//   getSpeakers() {
+//     return this.load().map((data: any) => {
+//       return data.speakers.sort((a: any, b: any) => {
+//         let aName = a.name.split(' ').pop();
+//         let bName = b.name.split(' ').pop();
+//         return aName.localeCompare(bName);
+//       });
+//     });
+//   }
 
-  getTracks() {
-    return this.load().map((data: any) => {
-      return data.tracks.sort();
-    });
-  }
+//   getTracks() {
+//     return this.load().map((data: any) => {
+//       return data.tracks.sort();
+//     });
+//   }
 
-  getMap() {
-    return this.load().map((data: any) => {
-      return data.map;
-    });
-  }
+//   getMap() {
+//     return this.load().map((data: any) => {
+//       return data.map;
+//     });
+//   }
 
 }
