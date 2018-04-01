41903955ba88dc5dd9a3c9d7356de8b83dd832b9
Laura Himelman <laura.himelman@coloradocollege.edu>
2018-04-01 18:27:10 +0000

    detached map tab

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 
 import { FeedPage } from '../feed/feed';
-import { MapPage } from '../map/map';
 import { AddPostPage } from '../addpost/addpost';
 import { ProfilePage } from '../profile/profile';
 
@@ -13,9 +12,8 @@ import { ProfilePage } from '../profile/profile';
 export class TabsPage {
 
   tab1Root = FeedPage;
-  tab2Root = MapPage;
-  tab3Root = AddPostPage;
-  tab4Root = ProfilePage;
+  tab2Root = AddPostPage;
+  tab3Root = ProfilePage;
 
   constructor() {
 
